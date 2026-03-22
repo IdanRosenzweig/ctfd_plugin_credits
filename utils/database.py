efbb2c83009a6c7ba336e08fbdba18b00b7cac9f
idanrosen <idanr@bindecy.com>
2026-03-22 18:20:58 +0200

    init

---
+++ utils/database.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, request, redirect, url_for, abort
+from CTFd.models import db
+from CTFd.utils.decorators import admins_only
+from CTFd.plugins import register_user_page_menu_bar, register_admin_plugin_menu_bar
+
+
+class Credits(db.Model):
+  __tablename__ = "credits"
+  
+  # credit id
+  id = db.Column(db.Integer, primary_key=True)
+  
+  # credit content
+  content = db.Column(db.Text)
+
+def add(
+    content
+  ) -> None:
+  new_credit = Credits(content=content)
+  db.session.add(new_credit)
+  db.session.commit()
+
+def remove(credit_id) -> None:
+  credit = Credits.query.filter_by(id=credit_id).first()
+  if credit:
+    db.session.delete(credit)
+    db.session.commit()
+
+def remove_all() -> None:
+  Credits.query.delete()
+  db.session.commit()
+  
+def get_all():
+  return Credits.query.all()
